26d3fbe4a3e38aa4dfcebd3beaabbd458cdeb4a8
itsmeshishir <motivate23a@gmail.com>
2022-06-14 12:12:14 +0545

    day 3 video where we chouse the installation of the //npx eslint --init// to chose the standard of the project of google, standard , aribnb and so no

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 const express = require('express')
-const app= express()
+const app = express()
 
-app.get('/', function(req, res){
-    return res.send("hello, me");
+app.get('/', function (req, res) {
+  return res.send('hello,me')
 })
 
-app.listen(3000, function(){
-            console.log('Server started on port 3000'); 
+app.listen(3000, function () {
+  console.log('Server started on port 3000')
 })
-
